e2c3823f85b3c67dea183f79ee830cf35aea704e
forest-green <744685426@qq.com>
2020-06-17 23:49:24 +0800

    使用Gatsby博客主题

---
+++ my-blog/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `我的博客`,
+    author: `Dong Zhiwen`,
+    siteUrl: `https://zwdong.cn/`,
+    description: `我的个人博客`,
+    social: [
+      // {
+      //   name: `twitter`,
+      //   url: `https://twitter.com/gatsbyjs`,
+      // },
+      {
+        name: `github`,
+        url: `https://github.com/forest-green`,
+      },
+    ],
+  },
+}
---
+++ my-blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Styled, thematicBreak } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+export default () => (
+  <thematicBreak>
+    上海 前端开发
+    <br />
+    初始化博客页面
+  </thematicBreak>
+)
+// export default function Bio() {
+//   return (
+//     <>
+//       This is where <Styled.a href="http://zwdong.cn/">Dong zhiwen</Styled.a>
+//       {` `}
+//       goes.
+//       <br />
+//       Or whatever, you make the rules.
+//     </>
+//   )
+// }
